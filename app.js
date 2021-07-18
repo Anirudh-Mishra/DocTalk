4743e690f1dcc08555b45567bcf9e0def8e6f7bc
unknown <mishraanirudhmail@gmail.com>
2021-07-18 22:03:15 +0530

    added config folder

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+// Bodyparser
+app.use(express.urlencoded ({ extended: false }))
+
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
